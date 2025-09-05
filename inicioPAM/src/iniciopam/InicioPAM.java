803190317a9b0a7c331fb75a8e7d8f50b65468a5
Mayy04 <124048822@upq.edu.mx>
2025-09-04 20:32:24 -0600

    creacion de clase INICIOPAM

--- inicioPAM/src/iniciopam/InicioPAM.java
+++ inicioPAM/src/iniciopam/InicioPAM.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package iniciopam;
-
+import javax.swing.JFrame;
 /**
  *
  * @author May
@@ -14,7 +14,10 @@ public class InicioPAM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        inicioGUI in= new inicioGUI();
+        in.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//cierra el programa cuando cierra ventana
+        in.setSize(500,200);//
+        in.setVisible(true);
     }
     
 }
