6ebe982e2eee27ae2a01f9049c5c66d1da35613a
Mayy04 <124048822@upq.edu.mx>
2025-09-18 20:25:53 -0600

    Creación de 3 scripts y un archivo html principal

---
+++ Practicas/ej2.js
@@ -0,0 +1,4 @@
+cuadrado = numero => numero*numero;
+console.log(cuadrado(11));
+console.log(cuadrado(1000000));
+console.log(cuadrado(35.7));
---
+++ Practicas/ej3.js
@@ -0,0 +1,3 @@
+const saludoPersonalizado=(nombre, edad) =>
+"Hola, mi nombre es " + nombre +" y tengo "+edad + "años. :D";
+console.log(saludoPersonalizado("Mayra", 19));
