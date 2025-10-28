c9e8b3d53c4b0df3a43d107b98cfd700df1c6b1d
Mayy04 <124048822@upq.edu.mx>
2025-10-27 23:47:35 -0600

    Práctica de scrollview

--- Intro/Screens/ScrollScreen.js
+++ Intro/Screens/ScrollScreen.js
@@ -1,13 +1,257 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, ScrollView } from 'react-native'
+import React, { Component } from 'react'
 
+export default function ScrollScreen(){
 
-export default function BotonesScreen(){
     return (
-      <View>
-        <Text>En construcción</Text>
+      <View style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={true} style={styles.scrollArea}>
+        <Text style={styles.title}>Mundo oscuro Pt. 1</Text>
+        <Text style={styles.subtitle}>Capitulo 1: Trabajo que hacer</Text>
+        <Text style={styles.text}>Voy conduciendo por la ciudad en busca de mi siguiente objetivo mientras el sol se 
+oculta para dar paso a la noche, no pienso matarlo como uno podría creer con la 
+palabra “objetivo”, soy un cazador de demonios que se dedica a deshacerme de los 
+demonios malos y ayudar a las criaturas mitológicas y a los que se convierten en 
+estas, Me gusta que me llamen Nick para que nadie sepa mi verdadero nombre que 
+es muy vergonzoso, aunque en la ciudad soy el chico de la pizza soy el que caza 
+demonios mientras todo duermen o toman una foto en el puesto de café más 
+cercano, tengo un aclamado lugar en el consejo del mundo alterno, inframundo, 
+mundo sombrío o como sea que le llamen las personas con las tantas historias 
+ficticias sobre los cazadores de demonios, hasta me resulta divertido entretenerme 
+con esas historias y ver en lo que aciertan y en lo que no. el día de hoy rescato a un 
+chico de 19 años cerca del lago de la ciudad que se ha convertido en un tritón, parte 
+de los míticos, como les llamamos a los que no son humanos, cazadores como yo 
+o demonios. El chico quedó atrapado en el lago Alice’s tears cuando trataba de 
+recolectar pruebas para hacer una limpieza al rio que ya no representa lo que su 
+nombre indica, antes el agua era tan pura como las lágrimas de Alicia Lidell, la figura 
+del pueblo gracias a una reedición de El país de las Maravillas que la sitúa en 
+Pedderton View y que tuvo éxito. Reconozco al chico sin haber llegado a él, me dio 
+un folleto para promover la limpieza del rio, incluso me ofrecí a firmar para la 
+recolección de firmas que hace muchas veces por lo que sé. Quien diría que se 
+convertiría en un mítico relacionado a lo que a él tanto lo apasiona, un tritón. El 
+parque de la ciudad sigue abierto y con una fiesta cerca de mi objetivo, sacarlo del 
+agua y traerlo al carro será de lo peor, entro al parque por la entrada de vehículos 
+sin checar mi tarjeta o hablar al que velador nocturno, es un mítico que prefirió seguir 
+con su vida normal y sabe que venir a una fiesta el sábado por las noches no es lo 
+mío y estoy aquí para rescatar a un mítico. {"\n"}</Text>
+        <Text style={styles.text}>— No me digas, niño rubio, otra emergencia —Dije que nos conocemos, no que 
+tenemos la mejor relación. El velador se saca el sombrero y veo como sus cuernos 
+aparecen sobre su cabeza, es un fauno que mis padres ayudaron en su momento y 
+lo ayudaron a reincorporarse al mundo moderno, lo que no es fácil por el shock de 
+convertirte en algo que pensabas que era cuentos de hadas y todo eso. A diferencia 
+de lo que se cree en la cultua popular, los verdaderos faunos son una combinación 
+de humano y ciervo, y no de humano y una cabra, es muy confuso por los rasgos 
+similares que tienen los faunos de la mitología y de la realidad.{"\n"}</Text>
+        <Text style={styles.text}>— ¿Cómo crees? Estoy aquí para unirme a la fiesta —como parte de nuestra 
+relación amistosa-conflictiva el sarcasmo es lo más común y nunca falta en nuestra 
+conversación. {"\n"}</Text>
+        <Text style={styles.text}>— Es más posible que te tomes un retiro para ir a la universidad a ir a cualquier 
+fiesta que entregues, ve por esa emergencia y vete lo más antes posible, montarán 
+un escenario en quince minutos y no les gustara ver algo fuera de lo normal. {"\n"}</Text>
+        <Text style={styles.text}>— Dame diez minutos, es un tritón en día de transformación, solo lo llevaré a la 
+mansión y le explicaré lo de siempre —le explico lo mismo de siempre para enojarlo 
+a propósito, sé que es malo hacerlo, pero cuando se enoja hace sonidos de ciervos 
+que con su voz aguda resultan graciosos. El velador asiente y yo me voy caminando 
+hacía el tritón del otro lado del río, lo bueno de tener un uniforme negro y rojo es 
+que es casi indetectable en la noche, perfecto para pasar desapercibido sin usar la 
+energía de invisibilidad que a la larga me termina agotando. {"\n"}</Text>
+        <Text style={styles.text}>Puedo ver al chico de piel color roble y cabello rizado tratando de ocultarse de los 
+chicos que toman cerveza y se acarician sin control en cualquier lugar, puedo 
+percibir sus emociones como si fueran un color, que en su caso es un verde grisáceo 
+indicando el terror y la sorpresa que siente por haberse transformado en un mítico 
+en un lugar tan público que antes creía seguro, me costará mucho hacer que pueda 
+entrar al bosque sin desmayarse por los terribles recuerdos de este día. A unos 
+metros del chico, una pareja de novios que no pueden soltar sus labios se acercan 
+más al tronco en donde el chico se está escondiendo, puedo sentir que su color 
+cambia a un tono más oscuro, está muy aterrado y no sabe qué hacer. En estos 
+momentos desearía que mi poder especial fuera las ilusiones como la chica de 
+Virginia y no la clarividencia enfocada a las emociones y energía de las personas, 
+podría aparecer un oso para espantarlos y recoger al chico. Tendré que hacerlo de 
+la manera manual. {"\n"}</Text>
+        <Text style={styles.text}>— da mihi facultatem iactare quasi fabulosa —invoco la fuerza del lanzamiento con 
+la frase de invocación en latín, donde surgió parte de nuestra mitología y lanzo una 
+roca cerca de donde están los chicos, haciendo un agujero en uno de los arboles 
+más cercanos, asusto de una manera exponencial a la pareja, pero logro que se 
+vayan y me dejen hacer mi trabajo. Finalmente llego al chico y me recuesto sobre 
+el tronco para ver que no le haré daño y explicarle como saldremos de aquí. {"\n"}</Text>
+        <Text style={styles.text}>— ¡Por favor no le digas a nadie! Me van a hacer algo, no sé qué soy —no es 
+necesario detectar sus emociones para saber que ya entró en un estado de shock.{"\n"} </Text>
+        <Text style={styles.text}>— Vengo a rescatarte, soy un cazador de demonios que ayuda a míticos como tú, 
+sé que suena muy loco, pero te lo explicaré al llegar a tu nuevo hogar mientras 
+aprendes sobre ti —le explico lo básico para que vea que soy un aliado y no un 
+enemigo, es lo más básico que se debe hacer en estos rescates para que el mítico 
+pueda confiar en quien lo rescata y no delatarse o acabar con su vida. {"\n"}</Text>
+        <Text style={styles.text}>— Pero no puedo salir, traté de salir y me quedé sin oxígeno —llora mientras veo 
+como mueve de manera desesperada su altea azul que brilla a la luz de la luna, es 
+un indicio sobre la facilidad que tendrá para enseñarlo a nadar de la manera 
+correcta.{"\n"} </Text>
+        <Text style={styles.text}>— Lo sé, en las primeras horas los tritones transformados se les dificulta respirar 
+con la nariz, se activa el instinto de respiración por las branquias y se les hace difícil 
+respirar por la nariz, pero en este caso respirarás con la boca sin parar y sin hablar, 
+puedes hacerlo por poco menos de una hora hasta que el oxígeno no sea suficiente, 
+tardaremos media hora en llegar a la mansión, dame tus brazos para sacarte. {"\n"}</Text>
+        <Text style={styles.text}>El chico me da sus brazos y al tener la fuerza en los brazos lo jalo con mucha 
+facilidad y lo cargo en mis brazos por el camino menos visible hacia mi carro 
+previamente protegido para no dejar ver nada de adentro y para evitar manchas por 
+líquidos. Han pasado ocho minutos, todavía tengo tiempo de llegar al carro antes 
+de que llegue el remolque para construir el innecesario escenario cuando ya hay 
+dos en el parque. El estacionamiento sigue igual de vacío, solo con el velador 
+juzgándome con la mirada, y con nadie normal estando por ahí meto al chico por el 
+asiento de copiloto y le doy una playera de manga larga para que sus branquias y 
+aleta dorsales no se ven, y le doy una pieza de tela con un material flexible y 
+resistente con forma de piernas humanas, normalmente tendría mi armamento de 
+rescate bien guardado, pero cuando hoy pensé que no iba a haber un rescate por 
+ser el día anterior a la temporada de transformación, donde la mansión parece un 
+hotel y que en menos de medio año está casi vacía, es muy raro ver que haya una 
+transformación antes del inicio de la temporada, así que tengo sombreros de copa, 
+cubrebocas para los faunos y bronceador para los plantarios, humanos vegetales 
+por así decirlo. Me despido del fauno y me voy del parque para iniciar con la primera 
+rehabilitación/sesión de aprendizaje del año, un quince de abril en donde suelo 
+hacer una pequeña fiesta con mis amigos míticos y unos cuantos que no lo son 
+tanto antes de enfocar todo mi esfuerzo en reacondicionar a los transformados a su 
+nuevo estilo de vida. {"\n"}</Text>
+        <Text style={styles.text}>— ¿Ese señor era una criatura ciervo? —me pregunta el chico más relajado y con 
+un color más sueva para mi vista. {"\n"}</Text>
+        <Text style={styles.text}>— Había mencionado que no puedes hablar para no malgastar el oxígeno, pero 
+estás parcialmente en lo correcto es un fauno que me conoce, me deja rescatar a 
+nuevos míticos como tú y usamos el sarcasmo como un nuevo idioma. {"\n"}</Text>
+        <Text style={styles.text}> — oye, un fauno es una combinación de un humano y una cabra. {"\n"}</Text>
+        <Text style={styles.text}>— No hables hasta que lleguemos y faltan unos quince minutos. Es un error 
+causado por malas interpretaciones, nosotros consideramos que los faunos tienen 
+rasgos de ciervos y no de venados, en nuestra existencia ningún cazador ha 
+registrado a ningún fauno con rasgos de cabra, créeme, tenemos nuestra versión 
+del bestiario, si quieres puedo darte una copia —es increíble ser el encargado de 
+ayudar a los míticos, es como pintar un lienzo en blanco de una manera diferente y 
+con resultados diferentes, antes era aburrido cazar un demonio por noche y después 
+pasar el resto del día teniendo clases en casa sin tener a nadie con quien hablar o 
+divertirse, ahora tengo un trabajo que me encanta y como algunos deciden quedarse 
+en la mansión tengo a varios acompañantes para no estar sólo en la mansión 
+mientras mis padres se encuentran en Escocia haciendo grandes eventos en donde 
+los líderes de los lideres organizan lo que se hace a nivel mundial y como combatir 
+a los demonios malos con más facilidad al incrementar poco a poco. Vuelvo a 
+detectar las emociones del chico y por primera vez desde hace tres años un mítico 
+se tranquiliza en cuestión de minutos y no después de horas. {"\n"}</Text>
+        <Text style={styles.text}>En cinco minutos llegamos a la mansión de los Starkthorne, la mansión familiar que 
+será totalmente mía cuando cumpla los veinticinco, solo faltan dos años y cinco 
+meses para eso. La mansión tiene un estilo victoriano por fuera, fue construida hace 
+muchos años y por leyes de la ciudad solo se permite renovarla sin cambiar ningún 
+color y mucho menos modernizarla por fuera, por dentro es una casa lujosa del siglo 
+XXI con una biblioteca, zonas de entrenamiento, mi sala de entretenimiento y una 
+piscina con un jardín al lado con un techo tapado con plantas colgantes para no 
+exponer lo que se vive adentro, la institución de aprendizaje del general Nick 
+Starkthorne para nuevos míticos. Tengo que entrar por un lado al no haber un garaje 
+unido a la casa y que por suerte está unido al jardín y la piscina para este tipo de 
+situaciones.  {"\n"}</Text>
+        <Text style={styles.text}>— Llegamos a casa, nos espera grandes cosas y espero que puedas aceptar a tu 
+nuevo yo —le doy la bienvenida oficial a la mansión para demostrarle que soy su 
+aliado, que lo apoyaré hasta que pueda surgir de las cenizas como un fénix. {"\n"}</Text>
+        <Text style={styles.subtitle}>Capitulo 2: El nuevo recluta</Text>
+        <Text style={styles.text}>Al llegar aviso a todos mis compañeros para recibir al nuevo miembro y ayudarme 
+con los efectos secundarios de su transformación. Saco al chico del carro y ahora 
+que no estamos a la vista de los normales le pido que se quite la camiseta para que 
+pueda respirar mejor en el agua, el chico muestra una confusión al ver que no tengo 
+signos de timidez o nervios y está en lo correcto, llevo tanto tiempo haciendo esto 
+que ya no tienes el miedo a decir cosas incoherentes o equivocarte al perfeccionar 
+mis técnicas muy seguido. Inmediatamente todos mis compañeros llegan al jardín 
+para ayudar al nuevo recluta, un sobrenombre común que le ponemos a los 
+transformados que llegan a la mansión. Jalo de una cuerda en mis pantalones y la 
+parte debajo de las rodillas cae al suelo sin hacer ruido, prefiero tener la posibilidad 
+de transformar mis pantalones en shorts a media batalla que sumergirme en la 
+piscina con pantalones de cuero, una experiencia que no es para nada disfrutable. {"\n"}</Text>
+        <Text style={styles.text}>— Supongo que tienes muchas preguntas y eso es muy normal, pero iniciemos con 
+una pregunta de mi parte, ¿cuál es tu nombre y cómo fue que te transformaste? No 
+estás obligado a contestar la segunda pregunta —muchos suelen evitarla por no 
+comprender nada de lo que pasa, cosa que es entendible por el estado de shock 
+que es de color marrón de un tono casi negro—. Si no la contestas no hay ningún 
+problema, es totalmente razonable. {"\n"}</Text>
+        <Text style={styles.text}>— Bueno, me llamo Leon Conner, supongo que me has visto tratando de salvar al 
+río. Yo… estaba tomando unas muestras de agua para mostrar que estaba 
+contaminada cuando empecé a sentir un dolor en mis piernas, sentía que me iba a 
+quedar sin piernas, entonces el dolor se extendió por todo el cuerpo y caí en la orilla 
+del agua. Creo que me desmayé porque antes de caer al agua era de día y al 
+despertar ya era muy tarde, y como vivo solo no tenía a quién llamar o pedir auxilio, 
+entonces vi que no podía salir del agua, ni pararme; lo intenté, pero sentía que me 
+quedaba sin aire, me fijé que no estaban mis piernas, en cambio solo tenía una cola 
+de pez. Estaba aterrorizado y cuando intenté subir al tronco noté que por alguna 
+razón tenía branquias, pensaba que era una alucinación, pero no lo es, ahora soy 
+un tritón por el resto de mi vida…eso creo.{"\n"}</Text>
+        <Text style={styles.text}>— Tengo que felicitarte por querer contar tu historia y sobre este suceso que a la 
+mayoría lo suele marcar de por vida, es muy valiente de tu parte, Leon. Ahora es mi 
+turno para introducirte a esta nueva perspectiva del mundo, en primer lugar, solo 
+quiero decirte que, a pesar de lo malo que puede sonar, esto no es para nada algo 
+negativo o un defecto. Si te soy sincero, siempre soñé con ser un tritón o por lo 
+menos tener un amigo tritón, pero los cazadores no podemos transformarnos en un 
+mítico y la mayoría de tritones que hemos recibido prefieren irse con los marinos. {"\n"}</Text>
+        <Text style={styles.text}>— Gracias, supongo. No sé si pueda entender todo esto, o si puedo transformarme 
+en humano por al menos unos minutos, la próxima semana va a iniciar el proceso 
+de la limpieza del río y no quiero faltar. {"\n"}</Text>
+        <Text style={styles.text}>— Oye, para eso estamos los cazadores, los ayudamos a adaptarse y protegemos 
+a todos de los demonios. Y en cuanto a tu evento, sí hay una manera de verte lo 
+más humano posible, pero no dura por mucho tiempo y tras contarte sobre lo más 
+básico de este lado del mundo vas a entender las preguntas más importantes sobre 
+tú nuevo yo y que puedes hacer tras completar tu proceso de adaptación. Iniciemos 
+con lo básico, ¿Quiénes somos nosotros, los cazadores, míticos y demonios? {"\n"}</Text>
+        <Text style={styles.text}>— ¿Con que este es el nuevo recluta? Felicidades, me acercaría a la piscina si no 
+me dañara el agua —el primero en saludar a Leon fue el primero en quedarse en la 
+mansión, el único demonio bueno, o demonio caído en nuestra mitología, Josh. Él 
+descubrió que era un demonio caído a los quince y fue la primera persona a la que 
+ayudé a adaptarse, como suele pasar con la mayoría de su especie, tienen una tez 
+pálida, cabello oscuro o blanco en ciertos casos, una consciencia que se puede 
+volver inestable con facilidad y un miedo por las cosas puras como el agua de mar, 
+objetos de poder de los cazadores y la sal. Fue muy difícil adaptarlo porque vivía 
+con el trauma de ser un demonio en una familia de fieles creyentes que llegaban a 
+bañarlo dos veces en agua bendita que es pura y lo quemaba tan pronto hacía 
+contacto con su piel. {"\n"}</Text>
+    
+        </ScrollView>
       </View>
     )
-  
-}
+  }
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems:'center'
+  },
+  scrollArea:{
+    flex:1,
+    width:'65%',
+  },
+  item:{
+    backgroundColor:'#c9a0ff',
+    color:'#fff', 
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 10,
+    textAlign: 'center',
+  },
+  scrollContent:{
+    alignItems:'center',
+    paddingVertical:20,
+  },
+  title:{
+    fontSize:30,
+    fontWeight:'bold',
+    color: 'skyblue',
+    textAlign: 'center',
+    marginBottom:20,
+  },
+  subtitle:{
+    fontSize:22,
+    fontWeight:'bold',
+    color:'white',
+    textAlign: 'center',
+    backgroundColor:'blue',
+    borderRadius:10,
+    marginVertical:12,
+    paddingVertical:12,
+    paddingHorizontal:12,
+    width:'95%',
+    alignSelf:'center',
+  },
+  text:{
+    fontSize:16
+  },
+})
